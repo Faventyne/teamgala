95a9e5259cea2a8b4990cf7f164b75c0f15a17ec
Faventyne <guitchat@gmail.com>
2017-12-12 17:12:02 +0100

    Adding methods to all Controllers : User, Activity and Organization

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -5,8 +5,57 @@
  * Date: 05/12/2017
  * Time: 00:49
  */
-
+use Form\UserForm;
+use Symfony\Component\HttpFoundation\Request;
+use Silex\Application;
+use Model\User;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Model\Activity;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class ActivityController
 {
+    //Creates an activity (limited to activity manager)
+    public function addActivityAction(Request $request, Application $app){
 
+    }
+
+    //Update activity (limited to activity manager)
+    public function modifyActivityAction(Request $request, Application $app){
+
+    }
+
+    //Delete activity (limited to HR)
+    public function deleteActivityAction(Request $request, Application $app){
+
+    }
+
+
+    //Displays all activities for current user
+    public function myActivitiesAction(Request $request, Application $app){
+        $repository = $app['orm.em']->getRepository(Activity::class);
+
+    }
+
+    //Display selected activity (all users)
+    public function viewAction(Request $request, Application $app){
+        $repository = $app['orm.em']->getRepository(Activity::class);
+
+    }
+
+    //Grade an activity (all users)
+    public function gradeAction(Request $request, Application $app){
+        $repository = $app['orm.em']->getRepository(Activity::class);
+
+    }
+
+    //Grade an activity (all users)
+    public function resultsAction(Request $request, Application $app){
+        $repository = $app['orm.em']->getRepository(Activity::class);
+
+    }
+
+    /*Optional : release an activity (all users)
+    public function resultsAction(Request $request, Application $app){
+        $repository = $app['orm.em']->getRepository(Activity::class);
+    */
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,7 +6,54 @@
  * Time: 00:48
  */
 
+use Form\UserForm;
+use Symfony\Component\HttpFoundation\Request;
+use Silex\Application;
+use Model\User;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Model\Activity;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class UserController
 {
+    //Adds user to current organization (limited to HR)
+    public function addUserAction(Request $request, Application $app){
+
+    }
+
+    //Logs current user
+    public function loginAction(Request $request, Application $app){
+
+    }
+
+    //Displays the menu in relation with user role
+    public function homeAction(Request $request, Application $app){
+
+    }
+
+    // Creates user position and weight
+    public function addPositionWeightAction(Request $request, Application $app){
+
+    }
+
+    // Updates user position and weight
+    public function modifyPositionWeightAction(Request $request, Application $app){
+
+    }
+
+    // Modify user info
+    public function modifyUserAction(Request $request, Application $app){
+
+    }
+
+    // Delete user
+    public function deleteUserAction(Request $request, Application $app){
+
+    }
+
+    // Display all users
+    public function getAllUsersAction(Request $request, Application $app){
+
+    }
+
 
 }
