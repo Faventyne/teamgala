7f62b38ccf837f7d503a0983bec97ac45947c717
leClaude <alexandre.claudon@gmail.com>
2017-12-13 12:04:20 +0100

    Add modify pwd route

--- src/controllers.php
+++ src/controllers.php
@@ -8,6 +8,10 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
+// Set / Modify Password page
+$routePwd = $app->get('/password/{token}', "Controller\UserController::modifyPwdAction") ;
+$routePwd->bind('password') ;
+
 // Login page
 $routeLogin = $app->get('/', "Controller\UserController::loginAction") ;
 $routeLogin->bind('login') ;
